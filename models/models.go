1e865f89bf091f601d69287075b5b6b60b770bf6
cjg <1904772023@qq.com>
2022-05-23 18:51:14 +0800

    111

---
+++ models/models.go
@@ -0,0 +1,92 @@
+package models
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+
+	"gin-blog/pkg/setting"
+)
+
+var db *gorm.DB
+
+type Model struct {
+	ID int `gorm:"primary_key" json:"id"`
+	CreatedOn int `json:"created_on"`
+	ModifiedOn int `json:"modified_on"`
+}
+func (model *Model) BeforeCreate(tx *gorm.DB) error {
+    tx.Statement.SetColumn("CreatedOn", time.Now().Unix())
+
+    return nil
+}
+
+func (model *Model) BeforeUpdate(tx *gorm.DB) error {
+    tx.Statement.SetColumn("ModifiedOn", time.Now().Unix())
+
+    return nil
+}
+
+func init() {
+	var (
+		err error
+		dbName, user, password, host,tablePrefix string
+	)
+
+	sec, err := setting.Cfg.GetSection("database")
+	if err != nil {
+		log.Fatal(2, "Fail to get section 'database': %v", err)
+	}
+
+	// dbType = sec.Key("TYPE").String()
+	dbName = sec.Key("NAME").String()
+	user = sec.Key("USER").String()
+	password = sec.Key("PASSWORD").String()
+	host = sec.Key("HOST").String()
+	tablePrefix = sec.Key("TABLE_PREFIX").String()
+
+	db, err = gorm.Open(
+		mysql.Open( 
+			fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
+			user,
+			password,
+			host,
+			dbName)),&gorm.Config{
+				Logger: logger.Default.LogMode(logger.Info),
+				NamingStrategy: schema.NamingStrategy{
+					TablePrefix:   tablePrefix, // 表名前缀，`Tag` 的表名应该是 `blog_tag`
+					SingularTable: true,    // 使用单数表名，启用该选项，此时，`Tag` 的表名应该是 `blog_tag`
+				},
+
+			})
+		
+
+	if err != nil {
+		log.Println(err)
+	}
+	sqlDB,err := db.DB()
+	if err != nil {
+		log.Println(err)
+	}
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+
+
+	// db.SingularTable(true)
+	// db.LogMode(true)
+	// db.DB().SetMaxIdleConns(10)
+	// db.DB().SetMaxOpenConns(100)
+}
+
+// func CloseDB() {
+// sqlDB, err := db.DB()
+// defer sqlDB.Close()
+// if err != nil {
+// fmt.Println(err)
+// }
+// }
---
+++ models/tag.go
@@ -0,0 +1,70 @@
+package models
+
+type Tag struct {
+	Model
+
+	Name       string `json:"name"`
+	CreatedBy  string `json:"created_by"`
+	ModifiedBy string `json:"modified_by"`
+	State      int    `json:"state"`
+}
+
+// func (tag *Tag) BeforeCreate(tx *gorm.DB) error {
+//     tx.Statement.SetColumn("CreatedOn", time.Now().Unix())
+
+//     return nil
+// }
+
+// func (tag *Tag) BeforeUpdate(tx *gorm.DB) error {
+//     tx.Statement.SetColumn("ModifiedOn", time.Now().Unix())
+
+//     return nil
+// }
+func GetTags(pageNum int, pageSize int, maps interface{}) (tags []Tag) {
+	db.Where(maps).Offset(pageNum).Limit(pageSize).Find(&tags)
+
+	return
+}
+
+func GetTagTotal(maps interface{}) (count int64) {
+	db.Model(&Tag{}).Where(maps).Count(&count)
+
+	return
+}
+
+func ExistTagByName(name string) bool {
+	var tag Tag
+	db.Select("id").Where("name = ?", name).First(&tag)
+
+	return tag.ID > 0
+}
+
+func AddTag(name string, state int, createdBy string) bool {
+	db.Create(&Tag{
+		Name:      name,
+		State:     state,
+		CreatedBy: createdBy,
+	})
+
+	return true
+}
+
+func ExistTagByID(id int) bool {
+    var tag Tag
+    db.Select("id").Where("id = ?", id).First(&tag)
+   
+	return tag.ID > 0 
+
+}
+
+func DeleteTag(id int) bool {
+    db.Where("id = ?", id).Delete(&Tag{})
+
+    return true
+}
+
+func EditTag(id int, data interface {}) bool {
+    db.Model(&Tag{}).Where("id = ?", id).Updates(data)
+
+    return true
+}
